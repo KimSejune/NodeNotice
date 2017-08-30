3209c6ce0521c3da36db6480e23a1a89fbf8fb04
kimsejune <tpwns1088@gmail.com>
2017-08-30 21:50:56 +0900

    게시판 글 추가기능 구현

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const basicAuth = require('express-basic-auth')
 const bodyParser = require('body-parser')
-// const uuidv4 = require('uuid/v4')
+const uuidv4 = require('uuid/v4')
 const app = express()
 
 
 const datas = [
   {
-    id: '1',
+    id: uuidv4(),
     writing: 'my name is sejune kim'
   }
 ]
@@ -25,10 +25,12 @@ app.set('view engine', 'ejs')
 
 app.use('/static', express.static('public'))
 
+// 초기값 읽어오기
 app.get('/', (req, res) => {
   res.render('index.ejs', {datas})
 })
 
+// 게시판 댓글달기 페이지 이동
 app.get('/data/:id', (req, res) => {
   const data = datas.find(d => d.id === req.params.id)
   if(data){
@@ -39,6 +41,22 @@ app.get('/data/:id', (req, res) => {
   }
 })
 
+// 게시판 추가
+app.post('/data', bodyParserMiddleWare, (req,res) => {
+  const writing = req.body.writing
+
+  if(writing){
+    const data = {
+      id: uuidv4(),
+      writing
+    }
+    datas.push(data)
+    res.redirect('/')
+  }else {
+    res.status(400)
+    res.send('400 Bad Requset')
+  }
+})
 
 app.listen('3100', () => {
   console.log('NodeNotice Server Start!!!!!!!!!')
